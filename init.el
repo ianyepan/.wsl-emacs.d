794e7ebcc004146a1fc4a19d8d09c5aa923e1da6
ianpan870102 <ianpan870102@gmail.com>
2020-07-17 11:54:51 +0800

    Minor tweak for vintage cursor use

--- init.el
+++ init.el
@@ -314,7 +314,8 @@
     (save-buffer)
     (kill-this-buffer))
   :config
-  (setq evil-emacs-state-cursor '(hbar . 6))
+  (setq-default cursor-type  '(hbar . 5))
+  (setq evil-emacs-state-cursor '(hbar . 5))
   (with-eval-after-load 'evil-maps
     (define-key evil-normal-state-map (kbd "gd") #'xref-find-definitions)
     (define-key evil-normal-state-map (kbd "<f12>") #'xref-find-definitions)

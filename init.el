42a3b306836652b680cec257b268b0f1c1f2177c
Ian Y.E. Pan <ianpan870102@gmail.com>
2021-04-25 02:49:58 +0800

    evil-collection PR merged. Ivy/minibuffer ESC problem solved

--- init.el
+++ init.el
@@ -379,10 +379,7 @@
   :after evil
   :config
   (setq evil-collection-company-use-tng nil)
-  (evil-collection-init)
-  (with-eval-after-load 'ivy
-    (define-key ivy-mode-map (kbd "<escape>") nil)
-    (define-key ivy-minibuffer-map (kbd "<escape>") #'minibuffer-keyboard-quit)))
+  (evil-collection-init))
 
 (use-package evil-commentary
   :after evil

285876f87c0987394653308bd7a1aab5c1d3ac32
Ian Y.E. Pan <ianpan870102@gmail.com>
2021-05-10 00:30:07 +0800

    Use RJSX mode for javascript files (default js/jsx seems to be having trouble)

--- init.el
+++ init.el
@@ -634,6 +634,16 @@
 
 (use-package vimrc-mode)
 
+(use-package rjsx-mode
+  :mode ("\\.jsx?\\'" . rjsx-mode)
+  :custom-face
+  (js2-error   ((t (:inherit default :underscore nil))))
+  (js2-warning ((t (:inherit default :underscore nil))))
+  :config
+  (define-key rjsx-mode-map "<" nil)
+  (define-key rjsx-mode-map (kbd "C-d") nil)
+  (define-key rjsx-mode-map ">" nil))
+
 ;; (use-package web-mode
 ;;   :mode (("\\.html?\\'" . web-mode)
 ;;          ("\\.css\\'"   . web-mode)

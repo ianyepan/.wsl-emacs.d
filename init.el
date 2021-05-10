127e4f4674bd958017bbeec4de58bb3c60137596
Ian Y.E. Pan <ianpan870102@gmail.com>
2021-05-11 00:22:19 +0800

    For xref forward and backward jumps -- always recenter AND pulse (flash)

--- init.el
+++ init.el
@@ -260,6 +260,8 @@
 (use-package xref
   :ensure nil
   :config
+  (setq xref-after-jump-hook '(recenter xref-pulse-momentarily))
+  (setq xref-after-return-hook '(recenter xref-pulse-momentarily))
   (setq xref-prompt-for-identifier nil))
 
 (use-package zone
@@ -349,6 +351,7 @@
   (setq-default cursor-type  '(hbar . 5))
   (setq evil-emacs-state-cursor '(hbar . 5))
   (define-key evil-normal-state-map (kbd "C-w C-o") #'(lambda () (interactive) (neotree-hide) (delete-other-windows)))
+  (define-key evil-normal-state-map (kbd "C-o") #'(lambda () (interactive) (evil-jump-backward) (recenter) (xref-pulse-momentarily)))
   (global-set-key (kbd "C-x 1") #'(lambda () (interactive) (neotree-hide) (delete-other-windows)))
   (global-set-key (kbd "C-<up>") #'evil-scroll-line-up)
   (global-set-key (kbd "C-<down>") #'evil-scroll-line-down)

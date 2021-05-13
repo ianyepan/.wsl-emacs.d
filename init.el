a424da8fc19198785d70679e5ac403b2dfb6ebcd
Ian Y.E. Pan <ianpan870102@gmail.com>
2021-05-13 11:17:15 +0800

    No need for bleeding edge org mode

--- init.el
+++ init.el
@@ -11,7 +11,6 @@
 (require 'package)
 (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("org"   . "https://orgmode.org/elpa/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)

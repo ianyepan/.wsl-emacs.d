1b337c4a12b88d60278f7d28ef6fb641006117d4
Ian Y.E. Pan <ianpan870102@gmail.com>
2022-02-27 13:45:41 -0500

    Quicken ranger preview delay

--- init.el
+++ init.el
@@ -829,6 +829,7 @@ This follows the UX design of Visual Studio Code."
   :config
   (setq ranger-width-preview 0.5)
   (setq ranger-width-parents 0.125)
+  (setq ranger-preview-delay 0.02)
   (setq ranger-cleanup-eagerly t)
   (setq ranger-show-hidden t)
   (define-key ranger-mode-map (kbd "i") #'dired-toggle-read-only)

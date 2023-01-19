a98656bc82581d644645d4b13366888837c1baf5
Ian Y.E. Pan <ianpan870102@gmail.com>
2023-01-19 15:20:51 -0800

    Delete which-key and pdf-tools

--- init.el
+++ init.el
@@ -1128,16 +1128,6 @@ This follows the UX design of Visual Studio Code."
   (setq neo-show-hidden-files t)
   (setq neo-window-width 30))
 
-(use-package which-key
-  :config
-  (setq which-key-idle-delay 0.4)
-  (which-key-mode +1))
-
-;; (use-package pdf-tools
-;;   :mode (("\\.pdf\\'" . pdf-view-mode))
-;;   :config
-;;   (pdf-loader-install))
-
 ;; Org and LaTeX export
 ;;;; Ubuntu needs to have these installed:
 ;;;; 1. texlive-latex-extra

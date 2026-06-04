2584ab751ce2c2eb65d5250f20b41e7cbfe122a2
Ian Pan <ianpan870102@gmail.com>
2026-06-03 22:19:50 -0700

    Remove unused vterm configuration
    Zellij + Emacs is a much better workflow. Will not be using terminal
    integration in Emacs in foreseeable future.
    
    Signed-off-by: Ian Pan <ianpan870102@gmail.com>

--- init.el
+++ init.el
@@ -1214,42 +1214,6 @@ If not in a project, prompt user to enter initial dir."
   (define-key ranger-mode-map (kbd "C") #'dired-do-copy)
   (define-key ranger-mode-map (kbd "C-h") nil))
 
-;; Terminal integration
-
-;; (use-package vterm
-;;   :hook (vterm-mode . (lambda ()
-;;                         (setq-local global-hl-line-mode nil)
-;;                         (setq-local line-spacing nil)))
-;;   :preface
-;;   (defun ian/new-vterm-instance ()
-;;     (interactive)
-;;     (vterm t))
-;;   :config
-;;   (setq vterm-disable-bold t)
-;;   (setq vterm-timer-delay 0.01)
-;;   (with-eval-after-load 'evil
-;;     (evil-set-initial-state 'vterm-mode 'emacs))
-;;   (define-key vterm-mode-map (kbd "C-l") #'(lambda ()
-;;                                              (interactive)
-;;                                              (vterm-clear)
-;;                                              (vterm-clear-scrollback))))
-
-;; (use-package vterm-toggle
-;;   :after (projectile vterm evil)
-;;   :config
-;;   (setq vterm-toggle-fullscreen-p nil)
-;;   (setq vterm-toggle-scope 'project)
-;;   (add-to-list 'display-buffer-alist
-;;                '((lambda(bufname _) (with-current-buffer bufname
-;;                                       (or (equal major-mode 'vterm-mode)
-;;                                           (string-prefix-p vterm-buffer-name bufname))))
-;;                  (display-buffer-reuse-window display-buffer-at-bottom)
-;;                  (direction . bottom)
-;;                  (dedicated . t)
-;;                  (reusable-frames . visible)
-;;                  (window-height . 0.5)))
-;;   (global-set-key (kbd "C-`") #'vterm-toggle))
-
 ;; Misc
 
 (use-package gcmh

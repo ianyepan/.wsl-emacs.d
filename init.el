58ad92f4605f4d0abc9eb591af80ef35aff64008
Ian Pan <ianpan870102@gmail.com>
2026-06-04 02:02:21 -0700

    code folding: Change ellipsis to preferred format for builtin outline-minor-mode too
    Signed-off-by: Ian Pan <ianpan870102@gmail.com>

--- init.el
+++ init.el
@@ -1376,6 +1376,32 @@ If not in a project, prompt user to enter initial dir."
   (setq neo-show-hidden-files t)
   (setq neo-window-width 50))
 
+(use-package outline
+  :ensure nil
+  :preface
+  ;; Ref: https://www.jamescherti.com/emacs-customize-ellipsis-outline-minor-mode/
+  (defun ian/outline-set-buffer-local-ellipsis (ellipsis)
+    "Set a buffer-local ellipsis string ELLIPSIS for outline folding display.
+
+This function configures the current buffer to use a custom ellipsis string for
+selective display, typically in `outline-mode' or `outline-minor-mode'.
+
+The string ELLIPSIS is trimmed of trailing whitespace before use, as such
+whitespace can be misleading when lines are truncated or visually wrapped. In
+those cases, the trailing space may appear on a new visual line, creating the
+false impression of an additional line. Deleting this apparent line can
+inadvertently remove the entire folded logical line."
+    (let* ((display-table (or buffer-display-table (make-display-table)))
+           (face-offset (* (face-id 'shadow) (ash 1 22)))
+           (value (vconcat (mapcar (lambda (c)
+                                     (+ face-offset c))
+                                   (string-trim-right ellipsis)))))
+      (set-display-table-slot display-table 'selective-display value)
+      (setq buffer-display-table display-table)))
+  :config
+  (add-hook 'outline-minor-mode-hook
+            #'(lambda () (ian/outline-set-buffer-local-ellipsis " ▶ ..."))))
+
 (use-package outline-indent
   ;; The following keybindings work with evil-collection OOTB:
   ;; - zo: open fold
